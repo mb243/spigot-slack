db157d3494ff2f7e8ba36057892fe7a487ebaab9
Tad Taylor <bittiez@gmail.com>
2018-03-24 15:41:55 -0600

    Added SSReload command

--- src/main/java/US/bittiez/slackspigot/main.java
+++ src/main/java/US/bittiez/slackspigot/main.java
@@ -2,13 +2,19 @@ package US.bittiez.slackspigot;
 
 import com.ullink.slack.simpleslackapi.SlackSession;
 import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
-public class main extends JavaPlugin {
+public class main extends JavaPlugin implements Listener {
     private FileConfiguration config = getConfig();
     private SlackSession session;
 
@@ -24,6 +30,28 @@ public class main extends JavaPlugin {
         } catch (Exception e) {
             getLogger().log(Level.SEVERE, e.getMessage());
         }
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]) {
+        if (cmd.getName().equalsIgnoreCase("ssreload")) {
+            if (sender.hasPermission("spigotslack.reload")) {
+                this.reloadConfig();
+                config = getConfig();
+                sender.sendMessage("Config reloaded!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+
+    }
+
+    @EventHandler
+    public void onPlayerChatEvent(AsyncPlayerChatEvent e) {
+
     }
 
     private void createConfig() {

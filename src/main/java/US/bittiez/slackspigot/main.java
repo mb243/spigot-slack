cd5daaae58eef04472113d423fadd451dffa8b95
Tad Taylor <bittiez@gmail.com>
2018-03-24 16:29:48 -0600

    Added incoming message listener

--- src/main/java/US/bittiez/slackspigot/main.java
+++ src/main/java/US/bittiez/slackspigot/main.java
@@ -2,7 +2,11 @@ package US.bittiez.slackspigot;
 
 import com.ullink.slack.simpleslackapi.SlackChannel;
 import com.ullink.slack.simpleslackapi.SlackSession;
+import com.ullink.slack.simpleslackapi.SlackUser;
+import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
 import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
+import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -15,6 +19,7 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
@@ -22,6 +27,7 @@ public class main extends JavaPlugin implements Listener {
     private FileConfiguration config = getConfig();
     private SlackSession session;
     private SlackChannel chatChannel;
+    private SlackMessagePostedListener messagePostedListener;
 
     @Override
     public void onEnable(){
@@ -35,6 +41,33 @@ public class main extends JavaPlugin implements Listener {
             session.connect();
 
             chatChannel = session.findChannelByName(config.getString("chat-channel"));
+
+            messagePostedListener = new SlackMessagePostedListener()
+            {
+                @Override
+                public void onEvent(SlackMessagePosted event, SlackSession session)
+                {
+                    SlackChannel channelOnWhichMessageWasPosted = event.getChannel();
+                    String messageContent = event.getMessageContent();
+                    SlackUser messageSender = event.getSender();
+
+                    List<String> channels = config.getStringList("incoming-channels");
+                    for (String chan : channels){
+                        if (channelOnWhichMessageWasPosted.getName() == chan){
+                            String formattedMsg = config.getString("incoming-chat-format")
+                                    .replace("[DISPLAYNAME]", messageSender.getUserName())
+                                    .replace("[MSG]", messageContent)
+                                    .replace("[CHANNEL]", channelOnWhichMessageWasPosted.getName());
+                            formattedMsg = ChatColor.translateAlternateColorCodes('&', formattedMsg);
+                            getServer().broadcastMessage(formattedMsg);
+                            break;
+                        }
+                    }
+                }
+            };
+            //add it to the session
+            session.addMessagePostedListener(messagePostedListener);
+
             enabled = true;
         } catch (Exception e) {
             getLogger().log(Level.SEVERE, e.getMessage());
@@ -99,6 +132,7 @@ public class main extends JavaPlugin implements Listener {
     private void sendMessageToSlack(SlackChannel chan, String msg){
         if(msg.length() < 1)
             return;
+        msg = ChatColor.stripColor(msg);
         session.sendMessage(chan, msg);
     }
 
